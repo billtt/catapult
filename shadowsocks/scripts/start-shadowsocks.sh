7b52c38c5b59d1e4b4e98f3ac8d4520f261302f2
Zhou Yuntao <billtt@gmail.com>
2015-12-07 15:47:47 +0800

    Change Google DNS to OpenDNS

--- shadowsocks/scripts/start-shadowsocks.sh
+++ shadowsocks/scripts/start-shadowsocks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 nohup /usr/bin/ss-redir -c /etc/shadowsocks.json -l 12345 &>> /var/log/ss-redir.log &
-nohup /usr/bin/ss-tunnel -c /etc/shadowsocks.json -l 53 -u -L 8.8.8.8:53 &>> /var/log/ss-tunnel.log &
+nohup /usr/bin/ss-tunnel -c /etc/shadowsocks.json -l 53 -u -L 208.67.222.222:53 &>> /var/log/ss-tunnel.log &
 
